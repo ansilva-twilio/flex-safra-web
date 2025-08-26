a2c920ffd9bd438231e66ca4aff3f3ff11fb369a
André Silva <ansilva@twilio.com>
2025-08-26 12:27:45 -0300

    fixing server.js

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/safra-flex-app'));
+app.use(express.static('./dist/safra-flex-app/browser'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/safra-flex-app/'}),
+    res.sendFile('index.html', {root: 'dist/safra-flex-app/browser'}),
 );
 
 // Start the app by listening on the default Heroku port
